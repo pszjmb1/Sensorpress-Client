a0cb08efe03fbf19a247a6aae367ccf5dba7a56d
Jesse Blum <jesse.blum@nottingham.ac.uk>
2012-04-18 10:50:10 +0100

    Now uses the XML-RPC protocol to select and insert into Shadowpress.

--- src/uk/ac/nottingham/horizon/jmb/shadowpress/client/SimpleProxyClient.java
+++ src/uk/ac/nottingham/horizon/jmb/shadowpress/client/SimpleProxyClient.java
@@ -20,7 +20,8 @@ package uk.ac.nottingham.horizon.jmb.shadowpress.client;
 
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.HashMap;
+
+import junit.framework.Assert;
 
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.xmlrpc.XmlRpcException;
@@ -29,6 +30,7 @@ import org.apache.xmlrpc.client.XmlRpcClientConfigImpl;
 import org.apache.xmlrpc.client.XmlRpcCommonsTransportFactory;
 
 public class SimpleProxyClient {
+	
 	public void getData(String url, String user, String password, String proxy_host, String proxy_port){
 		XmlRpcClientConfigImpl config = new XmlRpcClientConfigImpl();
 	    try {
@@ -63,14 +65,99 @@ public class SimpleProxyClient {
 			e.printStackTrace();
 		}
 	}
+	
+	private XmlRpcClient setupProxyConnection(String url, String proxy_host, String proxy_port){
+		XmlRpcClientConfigImpl config = new XmlRpcClientConfigImpl();
+	    try {
+			config.setServerURL(new URL(url));
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+	    XmlRpcClient client = new XmlRpcClient();
+	    XmlRpcCommonsTransportFactory transportFactory  = new XmlRpcCommonsTransportFactory( client );
+	    client.setTransportFactory(transportFactory);
+	    HttpClient httpClient = new HttpClient();
+	    httpClient.getHostConfiguration().setProxy(proxy_host, new Integer(proxy_port));
+
+	    transportFactory.setHttpClient(httpClient);
+	    client.setTransportFactory( transportFactory );
+	    client.setConfig(config);
+	    return client;
+	}
+	
+	public Object[] selectFromXMLRPC(String url, String user, String password, String proxy_host, 
+			String proxy_port, String type, Integer limit){
+		
+		XmlRpcClient client = setupProxyConnection(url, proxy_host, proxy_port);
+	    Object[] params;
+	    if(limit > 0){
+	    	params = new Object[]{user,password,new String(type), limit};
+	    
+		    try {
+		    	return (Object[])client.execute("shadowpress.select", params);
+			} catch (XmlRpcException e) {
+				e.printStackTrace();
+			}
+	    }
+	    return null;
+	}
+	
+	public Object[] insertUsingXMLRPC(String url, String user, String password, String proxy_host, 
+			String proxy_port, String type, String value, Integer readingset_id, Integer reading_type){
+		
+		XmlRpcClient client = setupProxyConnection(url, proxy_host, proxy_port);
+	    Object[] params = new Object[]{user,password,type, 
+	    		value,readingset_id,reading_type};
+    
+	    try {
+	    	return (Object[])client.execute("shadowpress.insert_reading", params);
+		} catch (XmlRpcException e) {
+			e.printStackTrace();
+		}
+	    return null;
+	}
 
 	/**
 	 * @param args 0 = url , 1 = user name, 2 = password, 3 = proxy host, 4= proxy port
 	 */
 	public static void main(String[] args) {
+		
 		SimpleProxyClient sc = new SimpleProxyClient();
+
+		String url = args[0];
+		String user = args[1];
+		String pwd = args[2];
+		String proxyurl = args[3];
+		String port  = args[4];
+		String type = args[5];
+		String limit  = args[6];
+		
 		//sc.getData("http://192.168.56.101/wordpress/xmlrpc.php");
-		sc.getData(args[0],args[1],args[2], args[3], args[4]);
+		//sc.getData(args[0],args[1],args[2], args[3], args[4]);
+		/*Object[] results = sc.selectFromXMLRPC("http://192.168.56.102/xmlrpc.php",//"http://192.168.56.101/wordpress/xmlrpc.php", 
+				"admin","a","reading",10);//"admin","qwerty","reading",10);*/
+		Object[] results = sc.selectFromXMLRPC(url, user, pwd, proxyurl, port, type , Integer.valueOf(limit) );
+		
+		System.out.println(url + ","+user);
+		if(null != results){
+			System.out.println(results.length);
+			for(int i = 0; i < results.length; i++){
+				//System.out.println(results[i].getClass());
+				System.out.println(results[i]);
+			}
+		}else{
+			System.out.println("null results");
+		}
+		
+		results = sc.insertUsingXMLRPC(
+				url, user, pwd, proxyurl, port,"dec_4_1","999.9",9999,9999);
+		Assert.assertEquals(1, results.length);
+		System.out.println(results.length);
+		for(int i = 0; i < results.length; i++){
+			System.out.println(results[i]);
+		}
 	}
 
 }
